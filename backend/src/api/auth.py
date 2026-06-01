6ac5e2c8672dc796f18c16ef6791cb0eb5b7e7e3
lakshmihasa_2003 <lakshmihasa2003@gmail.com>
2026-06-01 11:42:20 +0530

    Implement JWT login authentication

--- backend/src/api/auth.py
+++ backend/src/api/auth.py
@@ -1,3 +1,6 @@
+from src.schemas.user_schema import UserLogin
+from src.auth.password import verify_password
+from src.auth.jwt_handler import create_access_token
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
@@ -51,4 +54,40 @@ def register_user(
     return {
         "message": "User registered successfully",
         "user_id": new_user.id
+    }
+@router.post("/login")
+def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    token = create_access_token(
+        {
+            "sub": db_user.email,
+            "role": db_user.role
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
     }
