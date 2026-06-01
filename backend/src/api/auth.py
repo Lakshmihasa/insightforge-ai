e8209faf641fa592a1771ecca8c4068f113c7432
lakshmihasa_2003 <lakshmihasa2003@gmail.com>
2026-06-01 11:33:25 +0530

    Implement PostgreSQL user registration API

--- backend/src/api/auth.py
+++ backend/src/api/auth.py
@@ -1,4 +1,13 @@
 from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+
+from sqlalchemy.orm import Session
+
+from src.database.session import get_db
+from src.models.user import User
+from src.schemas.user_schema import UserRegister
+from src.auth.password import hash_password
 
 router = APIRouter(
     prefix="/auth",
@@ -10,4 +19,36 @@ router = APIRouter(
 def auth_test():
     return {
         "message": "Auth service running"
+    }
+
+
+@router.post("/register")
+def register_user(
+    user: UserRegister,
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User registered successfully",
+        "user_id": new_user.id
     }
